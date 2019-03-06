6bb86bd8cb4a1ad21106f79f47c616ed60b65d8d
Brett Bonnet <brettbonnet4@gmail.com>
2019-03-06 09:02:36 -0500

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -10,3 +10,20 @@ def caesar_cipher(str, num = 1)
 end
 
 print caesar_cipher("Helloz", 2)
+
+
+=begin
+def get_unicode(char)
+  (0..109_976).each do |pos|
+    chr = ''
+    chr << pos
+    return pos.to_s(16) if chr == char
+  end
+end
+
+def get_character(hexnum)
+  char = ''
+  char << hexnum.to_i(16)
+end
+
+=end
