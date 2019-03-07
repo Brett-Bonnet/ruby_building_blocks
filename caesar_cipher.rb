57acc75e517df10f59ec7a1266ab7be9bc6c2136
Brett Bonnet <brettbonnet4@gmail.com>
2019-03-07 06:42:53 -0500

    Added comments

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,7 @@
 
 def caesar_cipher(string, shift_num=1)
-hex_array = string.split('').map {|letter| letter = letter.ord }  
-char_array = hex_array.map {|num| num > 64 ? shift_hex(num, shift_num) : num.chr }
+hex_array = string.split('').map {|letter| letter = letter.ord } #find hex number for each letter  
+char_array = hex_array.map {|num| num > 64 ? shift_hex(num, shift_num) : num.chr } 
 cipherized = char_array.join('')
 
 puts cipherized
@@ -10,12 +10,12 @@ end
 def shift_hex(hexnum, shift)
 shifted = hexnum + shift
   if shifted > 122
-    (shifted -= 26).chr
+    (shifted -= 26).chr #Wrap lowercase letter
   else
     if shifted > 90 && shifted < 97
-      (shifted -= 26).chr
+      (shifted -= 26).chr #Wrap uppercase letter
     else 
-      shifted.chr
+      shifted.chr 
     end  
   end
 end
