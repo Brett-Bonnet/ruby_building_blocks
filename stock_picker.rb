ea76d30d438a64624f46393fbadd246a7af26d47
Brett Bonnet <brettbonnet4@gmail.com>
2019-03-10 19:33:06 -0400

    Created stock_picker.rb

---
+++ stock_picker.rb
@@ -0,0 +1,23 @@
+
+def stock_picker(days)
+
+buy_day = buy(days)
+
+
+
+end
+
+
+
+def buy(days)
+ buy_num = days.min
+ buy_index = days.index(buy_num)
+
+end
+
+def sell(days)
+ sell_day = days.each {}
+
+end
+
+puts stock_picker([12, 234, 5, 234, 45, 6776, 45, 345,])
