cf2894c2d91a8bb5ed2c5ca320f93ebe075ba093
Brett Bonnet <brettbonnet4@gmail.com>
2019-03-23 12:37:35 -0400

    Finished stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,19 @@
+def stock_picker(stocks)
+  buy_day = 0
+  sell_day = 0
+  most_profit = 0
 
-def stock_picker(days)
-
-buy_day = buy(days)
-
-
-
+  stocks[0..-1].each_with_index do |buy, day|
+    stocks[(day + 1)..-1].each do |sell|
+        if sell - buy > most_profit
+            most_profit = sell - buy
+            buy_day = stocks.index(buy)
+            sell_day = stocks.index(sell)
+        end
+    end
 end
 
+[buy_day, sell_day]
+  end
 
-
-def buy(days)
- buy_num = days.min
- buy_index = days.index(buy_num)
-
-end
-
-def sell(days)
- sell_day = days.each {}
-
-end
-
-puts stock_picker([12, 234, 5, 234, 45, 6776, 45, 345,])
+p stock_picker([17,3,6,9,15,8,6,1,10])
