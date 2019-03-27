7b8f6705330cbfb76411a73888db6fdfc2c0716a
Brett Bonnet <brettbonnet4@gmail.com>
2019-03-27 09:56:34 -0400

    Reduced substrings down to one function

--- substrings.rb
+++ substrings.rb
@@ -2,29 +2,24 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 
 def substrings(to_search, reference)
-  results = {}
-  found = []
   search_array = to_search.split(' ')
+  found = []
+  counts = Hash.new(0)
 
-  
   search_array.each do |x| 
-    reference.each do |word, count|
-      if x.downcase[word] then
+    reference.each do |word|
+      if x.downcase[word]
        found.push(word)
-     end
-  end
- end 
-count_results(found)
-end
+      end
+    end
+  end 
 
-def count_results(array)
-counts = Hash.new(0)
-array.each { |word| counts[word] += 1 }
-sorted_counts = Hash[counts.sort]
-sorted_counts
-end
+  found.each { |word| counts[word] += 1 }
+  sorted_counts = Hash[counts.sort]
 
+end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
+
 
 
